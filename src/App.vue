515c1dc38817514821ad884863e7dc1838775bbf
Lukas Fornaro <lukas@fornaro.de>
2018-01-12 22:50:23 +0100

    implemented navigation bottom bar
    npm install node-sass sass-loader for scss support

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,9 @@ export default {
 </script>
 
 <style>
+
+@import 'assets/scss/normalize';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="navigation">
-    {{ navigation }}
+    <a href="" @click="setActive()">Ich Ich Ich</a>
+    <a href="" @click="setActive()">Profil</a>
+    <a href="" @click="setActive()">Upload</a>
+    <a href="" @click="setActive()">Nachrichten</a>
   </div>
 </template>
 
@@ -11,10 +14,31 @@ export default {
     return {
       navigation: 'NAV BAR'
     }
+  },
+  methods: {
+    setActive() {
+      return true;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss"> // npm install node-sass sass-loader for scss support
+
+.navigation {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: black;
+  padding: 16px;
+  color: white;
+  a {
+    color: white;
+    padding: 24px;
+    text-decoration: none;
+    font-weight: bold;
+  }
+}
+
 </style>
