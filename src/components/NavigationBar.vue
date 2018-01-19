af772088f322ede9151b85e5ae3661b968257d4b
Lukas Sautter <lukas.sautter@gmx.de>
2018-01-19 12:54:41 +0100

    add AppHeader

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="navigation">
+    <a href="" @click="setActive()">Ich Ich Ich</a>
+    <a href="" @click="setActive()">Profil</a>
+    <a href="" @click="setActive()">Upload</a>
+    <a href="" @click="setActive()">Nachrichten</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar',
+  data () {
+    return {
+      navigation: 'NAV BAR'
+    }
+  },
+  methods: {
+    setActive() {
+      return true;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"> // npm install node-sass sass-loader for scss support
+
+.navigation {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: black;
+  padding: 24px 0;
+  color: white;
+  a {
+    color: white;
+    padding: 0 8px;
+    text-decoration: none;
+    font-weight: bold;
+  }
+}
+
+</style>
